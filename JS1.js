5922b90370d310c5ecaef9b2fd689c89afb71cbd
A Sin <124335907+choccolatte@users.noreply.github.com>
2024-11-02 02:20:15 +0530

    021124 0220

--- JS1.js
+++ JS1.js
@@ -3895,4 +3895,21 @@ allBtns.forEach(button =>{
 
 
 
-// 113. creating a small project using event handling. 
+// 113. creating a small project using event handling.
+const mainButton=document.querySelector('button')
+const body=document.body;
+const currentColor = document.querySelector('.current-color')
+
+function randomColorGenerator(){
+	const red=Math.floor(Math.random() * 256);
+	const green=Math.floor(Math.random() * 256);
+	const blue=Math.floor(Math.random() * 256);
+	const randomColor=`rgb(${red}, ${green}, ${blue})`
+	return randomColor;
+}
+mainButton.addEventListener('click', ()=>{
+	const randomColor=randomColorGenerator();
+	body.style.backgroundColor=randomColor;
+	currentColor.textContent=randomColor;
+})
+
